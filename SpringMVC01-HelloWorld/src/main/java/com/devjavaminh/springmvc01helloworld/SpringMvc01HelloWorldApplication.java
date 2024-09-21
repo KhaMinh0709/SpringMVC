ad7a4a026c9a0bd8285179503f799212341bdd50
KhaMinh <minh@gmail.com>
2024-09-21 15:27:43 +0700

    bai 01

---
+++ SpringMVC01-HelloWorld/src/main/java/com/devjavaminh/springmvc01helloworld/SpringMvc01HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.devjavaminh.springmvc01helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc01HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc01HelloWorldApplication.class, args);
+    }
+
+}
---
+++ SpringMVC01-HelloWorld/src/test/java/com/devjavaminh/springmvc01helloworld/SpringMvc01HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjavaminh.springmvc01helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc01HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
